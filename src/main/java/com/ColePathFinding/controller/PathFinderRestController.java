f722a8c0f783f9f15fe8bec00294985ee1f15018
Cole A. Kendall <mrcoole7890@outlook.com>
2022-05-11 22:08:06 -0400

    add blank post mapping method

--- src/main/java/com/ColePathFinding/controller/PathFinderRestController.java
+++ src/main/java/com/ColePathFinding/controller/PathFinderRestController.java
@@ -5,6 +5,8 @@ import java.util.Stack;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ProtoPathing.cole.MyList.ListType;
@@ -25,6 +27,11 @@ public class PathFinderRestController {
 			finalPath += pm.toString() + ",";
 		}
 		return finalPath;
+	}
+	
+	@PostMapping("/findpath")
+	String handel(@RequestBody String map) {
+		return map;
 	}
 	
 }
