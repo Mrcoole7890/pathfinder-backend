357959fa2780dc94f877db9c9976cff8d88c0804
Cole A. Kendall <mrcoole7890@outlook.com>
2022-06-05 22:52:33 -0400

    add changes to remote

--- src/main/java/com/ColePathFinding/controller/PathFinderRestController.java
+++ src/main/java/com/ColePathFinding/controller/PathFinderRestController.java
@@ -12,39 +12,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ProtoPathing.cole.MyList.ListType;
 import ProtoPathing.cole.ProtoSearchAgent;
+import ProtoPathing.cole.ProtoSearchAgentAStar;
 import ProtoPathing.cole.ProtoMap;
 
 @CrossOrigin
 @RestController
 public class PathFinderRestController {
 
-	@GetMapping("/test")
-	String testHandel() {
-		Stack<ProtoMap> pathToGoal = new ProtoSearchAgent(ListType.Stack, "src/main/resources/TestMap.txt").getPathToGoal();
+	@GetMapping("/astar")
+	String getAStartPath() {
+		Stack<ProtoMap> pathToGoal = new ProtoSearchAgentAStar("src/main/resources/TestMap.txt").getPathToGoal();
 		String finalPath = new String();
 		finalPath += pathToGoal.get(0).getPlayerPosition()[0] + " " + pathToGoal.get(0).getPlayerPosition()[1] + ",";
 		finalPath += pathToGoal.get(0).getGoalPosition()[0] + " " + pathToGoal.get(0).getGoalPosition()[1] + ",";
 		for(ProtoMap pm: pathToGoal) {
 			finalPath += pm.toString() + ",";
 		}
+		System.out.println(finalPath);
 		return finalPath;
 	}
 	
-	@PostMapping("/findpath")
-	String handel(@RequestBody String map) {
-		ArrayList<String> mapArr = new ArrayList<String>();
-		for(String line: map.split("\n"))
-			mapArr.add(line);
-		
-		Stack<ProtoMap> pathToGoal = new ProtoSearchAgent(ListType.Stack, mapArr).getPathToGoal();
-		
+	@GetMapping("/dfs")
+	String getDFS() {
+		Stack<ProtoMap> pathToGoal = new ProtoSearchAgent( ListType.Stack ,"src/main/resources/TestMap.txt").getPathToGoal();
 		String finalPath = new String();
 		finalPath += pathToGoal.get(0).getPlayerPosition()[0] + " " + pathToGoal.get(0).getPlayerPosition()[1] + ",";
 		finalPath += pathToGoal.get(0).getGoalPosition()[0] + " " + pathToGoal.get(0).getGoalPosition()[1] + ",";
 		for(ProtoMap pm: pathToGoal) {
 			finalPath += pm.toString() + ",";
 		}
-		
+		System.out.println(finalPath);
+		return finalPath;
+	}
+	
+	@GetMapping("/bfs")
+	String getBFS() {
+		Stack<ProtoMap> pathToGoal = new ProtoSearchAgent( ListType.Queue ,"src/main/resources/TestMap.txt").getPathToGoal();
+		String finalPath = new String();
+		finalPath += pathToGoal.get(0).getPlayerPosition()[0] + " " + pathToGoal.get(0).getPlayerPosition()[1] + ",";
+		finalPath += pathToGoal.get(0).getGoalPosition()[0] + " " + pathToGoal.get(0).getGoalPosition()[1] + ",";
+		for(ProtoMap pm: pathToGoal) {
+			finalPath += pm.toString() + ",";
+		}
+		System.out.println(finalPath);
 		return finalPath;
 	}
 	
